f457ec23a05aa9de50c4d37857c6cb21b6b5e468
Chris Sampson <csamposn@gmail.com>
2025-03-05 17:16:38 -0500

    bbackup

--- scripts/gameManager.gd
+++ scripts/gameManager.gd
@@ -4,9 +4,10 @@ var rock1_scene = preload("res://entity/rock1.tscn")
 
 @onready var rockSpawnTimer = $RockSpawner
 @onready var enemyContainer = $Enemies
+@onready var spawn_area:Area2D = $SpawnArea
 
 var spawn_interval: int = 10
-var entities_spawned:int = 3
+var entities_spawned:int = 1
 
 func _ready() -> void:
 	rockSpawnTimer.start(1)
@@ -15,6 +16,7 @@ func _ready() -> void:
 func spawn_rock():
 	print("spawned rock")
 	var rock = rock1_scene.instantiate()
+	rock.set_spawn_area(spawn_area)
 	enemyContainer.add_child(rock)
 	pass
 	
@@ -25,3 +27,7 @@ func _on_rock_spawner_timeout() -> void:
 		spawn_rock()
 	entities_spawned += 1
 	rockSpawnTimer.start(spawn_interval)
+
+#kill anything out of bounds
+func _on_spawn_area_body_exited(body: Node2D) -> void:
+	body.queue_free()
--- scripts/rock.gd
+++ scripts/rock.gd
@@ -5,11 +5,9 @@ extends Area2D
 var direction = Vector2.ZERO
 var target_varience = 1000
 var rotation_rate:int
-
+var spawn_area:Area2D
 @onready var sprite = $Sprite2D
 @onready var collider = $Collider
-
-
 
 # default behaivor
 	# random speed
@@ -21,6 +19,7 @@ func _ready() -> void:
 	randomize()
 	var player = get_tree().get_first_node_in_group("player").global_position
 	position = Vector2(0,0)
+	set_random_speed()
 	set_random_rotation()
 	set_random_location()
 	set_random_target(player)
@@ -34,22 +33,26 @@ func _process(delta: float) -> void:
 #	spin sprite and collision as one
 	sprite.rotation += rotation_rate * delta
 	collider.rotation += rotation_rate * delta
-	
+
+
 
 #-------------------------random state generators--------------------------
 
 #sets speed to random int
-func set_random_speed(max:int) -> void:
-	speed = randi() % 5
+func set_random_speed() -> void:
+	speed = randf_range(0,5)
 
 #applies varience to the direction based around the players current location
 func set_random_target(base:Vector2) -> void:
 	direction = global_transform.basis_xform(base)
 	
 func set_random_location() -> void:
-	var view = get_viewport().size
-	var rx = randi() % view.x + target_varience
-	var ry =  randi() % view.y + target_varience
+	var box = spawn_area.get_node("CollisionShape2D")
+	var view = get_viewport().size #get area of view
+	var outside = box.shape.get_rect().size
+	print(outside)
+	var rx = randi_range(view.x, outside.x)
+	var ry =  randi_range(view.y, outside.y)
 	global_position = Vector2(rx,ry)
 	pass
 
@@ -62,7 +65,6 @@ func set_random_scale() -> void:
 
 #--------------------------kill off screen-----------------------
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	queue_free()
 	pass
 	
 
@@ -84,3 +86,9 @@ func _on_body_entered(body: Node2D) -> void:
 		else:
 			direction.y *= -1
 	pass # Replace with function body.
+
+#--------------getters and setters--------------
+
+func set_spawn_area(area:Area2D) -> void:
+	spawn_area = area
+	pass
